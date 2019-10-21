6a1acdf5b47696ca0dcbf42f229f5a7fcaadca99
Maruf Ahmed <maruf.d99@gmail.com>
2019-10-21 16:55:39 +0100

    Pig latin

--- exercise_1.rb
+++ exercise_1.rb
@@ -1,20 +1,12 @@
-# Write a program that takes a word and returns the pig latin version.
-# Here are the (simplified) rules of pig latin:
-#
-# If the word starts with consonant, move it to the end and then add 'ay'
-# Or, if the word starts with a vowel, add 'way' to the end
-# e.g. dog  -> ogday
-#      up   -> upway
-#
-# Don't worry about handling multiple consonants
-# Assume the words are lowercase.
-# Assume your user is a developer — e.g. you don't need to write a user interface.
-
 def pig_latin(word)
-
-end
-
-## Tests:
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+      "#{word}way"
+  elsif word << first_letter
+    x = word [1..-1]
+    "#{x}ay"
+    end
+  end
 
 require_relative './helpers/assert_equal'
 assert_equal(pig_latin('dog'), 'ogday')
